e0f8044102f8a4fdcc60fb07692306e539d4367b
Benjamin Greene <b.greene@wustl.edu>
2018-03-26 22:31:42 -0400

    added palette tableview and cells, additional functionality

--- DesignJournal/PaletteCreatorViewController.swift
+++ DesignJournal/PaletteCreatorViewController.swift
@@ -8,37 +8,92 @@
 
 import UIKit
 
-class PaletteCreatorViewController: UIViewController, UIGestureRecognizerDelegate {
+class swatchCell: UITableViewCell  {
+    
+    @IBOutlet var colorLabel: UILabel!
+    var color: UIColor!
+    
+    
+}
 
+class PaletteCreatorViewController: UIViewController, UIGestureRecognizerDelegate, UITableViewDataSource {
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var colorBarView: UIView!
-    @IBOutlet var pixelLabel: UILabel!
+    @IBOutlet var table: UITableView!
+    var palettePreview: UIView!
     var change = false
-
+    
+    var fakeColors: [UIColor] = [UIColor.blue, UIColor.magenta, UIColor.purple]
+    
+    var numRows = 1
+    
+    
+    @IBOutlet var addRowButton: UIButton!
+    
     func handleTap(withSender gestureRecognizer: UITapGestureRecognizer) {
-        print("in handleTap()")
         var point: CGPoint?
         if gestureRecognizer.state == UIGestureRecognizerState.recognized {
             point = gestureRecognizer.location(in: gestureRecognizer.view)
-            print(point!)
         }
         
-        let pixelColor = getPixelColorAtPoint(point: point!, sourceView: imageView)
-        print(pixelColor)
-        colorBarView.backgroundColor = pixelColor
+        colorBarView.backgroundColor = getPixelColorAtPoint(point: point!, sourceView: imageView)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imageView.image = UIImage(named: "dtla-pink")
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(withSender: )))
         
+        imageView.backgroundColor = UIColor.black
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(withSender: )))
         imageView.addGestureRecognizer(tap)
         imageView.isUserInteractionEnabled = true
-        // Do any additional setup after loading the view.
+        
+        palettePreview = UIView(frame: CGRect(x: 0, y: 20, width: 400, height: 30 ))
+        setPalettePreview(colors: [UIColor.white,UIColor.lightGray, UIColor.darkGray, UIColor.black])
+        view.addSubview(palettePreview)
+        
     }
 
+    @IBAction func addSwatchRow(_ sender: Any) {
+        let indexPath: IndexPath = IndexPath(row: numRows, section: 0)
+        numRows += 1
+        
+        table.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    
+    
+    
+    /**
+        make sure to remove sublayers before calling again(if user adds/changes selections)
+    */
+    func setPalettePreview(colors: [UIColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = palettePreview.bounds
+        
+        var colorArray: [CGColor] = []
+        var locationArray: [NSNumber] = []
+        for(index, color) in colors.enumerated() {
+            colorArray.append(color.cgColor)
+            colorArray.append(color.cgColor)
+            locationArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index)))
+            locationArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index + 1)))
+        }
+        
+        gradientLayer.colors = colorArray
+        gradientLayer.locations = locationArray
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        
+        palettePreview.backgroundColor = UIColor.clear
+        palettePreview.layer.addSublayer(gradientLayer)
+        
+        palettePreview.layer.masksToBounds = true
+    }
     
     
     func getPixelColorAtPoint(point: CGPoint, sourceView: UIImageView) -> UIColor {
@@ -60,6 +115,38 @@ class PaletteCreatorViewController: UIViewController, UIGestureRecognizerDelegat
         }
         return color!
     }
+    
+    
+    /**
+     TableViewDataSource Methods
+    */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "swatchCell")! as! swatchCell
+        
+        cell.color = colorBarView.backgroundColor
+        cell.colorLabel.backgroundColor = cell.color
+        
+        
+        cell.textLabel?.text = String("#towel")
+        cell.textLabel?.textAlignment = NSTextAlignment(rawValue: 0)!
+        
+        let borderView: UIView = UIView()
+        borderView.layer.borderColor = UIColor.white.cgColor
+        borderView.layer.borderWidth = 1
+        borderView.layer.backgroundColor = UIColor.clear.cgColor
+        cell.selectedBackgroundView = borderView
+        
+        return cell
+        
+    }
+    
+    
+    
+    
     
     @IBAction func goBack(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
